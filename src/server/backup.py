03df65f189406c0faac01557a6c414147a15d524
Nyagami <hoangquan05112002@gmail.com>
2023-06-11 13:42:32 +0700

    use class to wrap backup functions

--- src/server/backup.py
+++ src/server/backup.py
@@ -47,92 +47,95 @@ class RestoreResponsePackage:
            'total': self.total,
         }
 
+class BackupHandler:
+    def backup(self, json_obj):
+        request = BackupRequestPackage(self.backup_dir, json_obj)
+        os.makedirs(request.dir, exist_ok=True)
+        if request.encoding == 'base64':
+            with open(request.path, 'wb') as f:
+                f.write(base64.b64decode(request.content))
+        else:
+            with open(request.path, 'w', encoding='utf-8') as f:
+                if request.encoding == 'utf-8':
+                    f.write(request.content)
+                else:
+                    f.write(json.dumps(request.content, ensure_ascii=False))
+        self.window.log(f'<span style="color: rgb(170, 241, 175);">{request.task}</span>: {request.path}')
+        return {"success": True, "message": "ok"}
 
-def backup(server, json_obj):
-    request = BackupRequestPackage(server.backup_dir, json_obj)
-    os.makedirs(request.dir, exist_ok=True)
-    if request.encoding == 'base64':
-        with open(request.path, 'wb') as f:
-            f.write(base64.b64decode(request.content))
-    else:
-        with open(request.path, 'w', encoding='utf-8') as f:
-            if request.encoding == 'utf-8':
-                f.write(request.content)
-            else:
-                f.write(json.dumps(request.content, ensure_ascii=False))
-    server.window.log(f'<span style="color: rgb(170, 241, 175);">{request.task}</span>: {request.path}')
-    return {"success": True, "message": "ok"}
-
-def restore(server, json_obj):
-    finished = json_obj.get('finished')
-    if finished:
-        server.window.log('Restore successfully')
-        return
-    index = int(json_obj.get('index'))
-    res = RestoreResponsePackage(server.meta[index], len(server.meta))
-    server.window.log(f'<span style="color: rgb(170, 241, 175);">{res.type}</span>: {res.real_path}')
-    return res.toJson()
-
-def old_restore(server, json_obj):
-    return {"success": False, "message": "Not Implemented"}
-
-def metadata(server):
-    meta = []
-    path = {
-        'Novel': os.path.join(server.backup_dir, 'Data', 'Novel.json'),
-        'Category': os.path.join(server.backup_dir, 'Data', 'Category.json'),
-        'NovelCategory': os.path.join(server.backup_dir, 'Data', 'NovelCategory.json'),
-        'Setting': os.path.join(server.backup_dir, 'Data', 'Setting.json'),
-        'Chapter': os.path.join(server.backup_dir, 'Data', 'Chapters'),
-        'Download': os.path.join(server.backup_dir, 'Data', 'Downloads'),
-        'Plugin': os.path.join(server.backup_dir, 'Data', 'Plugins'),
-        'Image': os.path.join(server.backup_dir, 'Images')
-    }
-
-    def walk(path: str, root: str):
-        if os.path.isfile(path):
-            yield path.removeprefix(root + '\\').replace('\\', '/')
+    def restore(self, json_obj):
+        finished = json_obj.get('finished')
+        if finished:
+            self.window.log('Restore successfully')
             return
-        for item in os.scandir(path):
-            for path in walk(item.path, root):
-                yield path
+        index = int(json_obj.get('index'))
+        res = RestoreResponsePackage(self.meta[index], len(self.meta))
+        self.window.log(f'<span style="color: rgb(170, 241, 175);">{res.type}</span>: {res.real_path}')
+        return res.toJson()
 
-    for type in ['Category', 'Novel', 'NovelCategory']:
-        meta.append({
-            'type': type,
-            'real_path': path[type]
-        })
+    def old_restore(self, json_obj):
+        return {"success": False, "message": "Not Implemented"}
 
-    for _path in walk(path['Chapter'], path['Chapter']):
-        meta.append({
-            'type': 'Chapter',
-            'real_path': os.path.join(path['Chapter'], _path)
-        })
-    
-    for type in ['Download', 'Plugin']:
-        for _path in walk(path[type], path[type]):
+    def metadata(self):
+        meta = []
+        path = {
+            'Novel': os.path.join(self.backup_dir, 'Data', 'Novel.json'),
+            'Category': os.path.join(self.backup_dir, 'Data', 'Category.json'),
+            'NovelCategory': os.path.join(self.backup_dir, 'Data', 'NovelCategory.json'),
+            'Setting': os.path.join(self.backup_dir, 'Data', 'Setting.json'),
+            'Chapter': os.path.join(self.backup_dir, 'Data', 'Chapters'),
+            'Download': os.path.join(self.backup_dir, 'Data', 'Downloads'),
+            'Plugin': os.path.join(self.backup_dir, 'Data', 'Plugins'),
+            'Image': os.path.join(self.backup_dir, 'Images'),
+            'Theme': os.path.join(self.backup_dir, 'Data', 'Theme.json')
+        }
+
+        def walk(path: str, root: str):
+            if os.path.isfile(path):
+                yield path.removeprefix(root + '\\').replace('\\', '/')
+                return
+            for item in os.scandir(path):
+                for path in walk(item.path, root):
+                    yield path
+
+        for type in ['Category', 'Novel', 'NovelCategory', 'Theme']:
             meta.append({
                 'type': type,
-                'encoding': 'utf-8',
-                'relative_path': _path,
-                'real_path': os.path.join(path[type], _path)
+                'real_path': path[type]
             })
+        if os.path.exists(path['Chapter']):
+            for _path in walk(path['Chapter'], path['Chapter']):
+                meta.append({
+                    'type': 'Chapter',
+                    'real_path': os.path.join(path['Chapter'], _path)
+                })
+        
+        for type in ['Download', 'Plugin']:
+            if os.path.exists(path[type]):
+                for _path in walk(path[type], path[type]):
+                    meta.append({
+                        'type': type,
+                        'encoding': 'utf-8',
+                        'relative_path': _path,
+                        'real_path': os.path.join(path[type], _path)
+                    })
+        if os.path.exists(path['Image']):
+            for _path in walk(path['Image'], path['Image']):
+                meta.append({
+                    'type': 'Image',
+                    'encoding': 'base64',
+                    'relative_path': _path,
+                    'real_path': os.path.join(path['Image'], _path)
+                })
 
-    for _path in walk(path['Image'], path['Image']):
         meta.append({
-            'type': 'Image',
-            'encoding': 'base64',
-            'relative_path': _path,
-            'real_path': os.path.join(path['Image'], _path)
+            'type': 'Setting',
+            'real_path': path['Setting']
         })
+        
+        meta_file = os.path.join(self.backup_dir, 'metadata.json')
 
-    meta.append({
-        'type': 'Setting',
-        'real_path': path['Setting']
-    })
-    
-    meta_file = os.path.join(server.backup_dir, 'metadata.json')
-
-    with open(meta_file, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(meta, ensure_ascii=False))
-    server.window.log('Backup successfully!')
+        with open(meta_file, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(meta, ensure_ascii=False))
+        self.window.log('Backup successfully!')
+        self.get_metadata()
--- src/server/server.py
+++ src/server/server.py
@@ -4,16 +4,9 @@ import asyncio
 import json
 import os
 
-from .backup import backup, restore, old_restore, metadata
+from .backup import BackupHandler
 
-map_handler = {
-    'Backup': backup,
-    'Restore': restore,
-    'OldRestore': old_restore,
-    'Metadata': metadata,
-}
-
-class WebSocketServer(threading.Thread):
+class WebSocketServer(threading.Thread, BackupHandler):
     def __init__(self, host, port, app_dir, window):
         super().__init__()
         self.host = host
@@ -23,6 +16,15 @@ class WebSocketServer(threading.Thread):
         self.window = window
 
         # this will avoid program re-read metadata file for each request
+        self.get_metadata()
+        self.map_handler = {
+            'Backup': self.backup,
+            'Restore': self.restore,
+            'OldRestore': self.old_restore,
+            'Metadata': self.metadata,
+        }
+
+    def get_metadata(self):
         metadata_path = os.path.join(self.backup_dir, 'metadata.json')
         if os.path.exists(metadata_path):
             with open(metadata_path, 'r', encoding='utf-8') as f:
@@ -32,13 +34,13 @@ class WebSocketServer(threading.Thread):
         async for message in websocket:
             json_obj = json.loads(message)
             handler_type = json_obj.get('type')
-            handler = map_handler.get(handler_type)
+            handler = self.map_handler.get(handler_type)
             if handler:
                 if handler_type == 'Metadata':
-                    handler(self)
+                    handler()
                     res = None
                 else:
-                    res = handler(self, json_obj.get('data'))
+                    res = handler(json_obj.get('data'))
             else:
                 res = {
                     "success": False,
